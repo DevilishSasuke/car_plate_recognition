0fde50b443e92237d4bdfe47000034f72d3fa41a
DevilishSasuke <petrovich_uasya@mail.ru>
2025-01-20 20:41:09 +0300

    testing processing photo methods

--- main.py
+++ main.py
@@ -7,10 +7,22 @@ target_size = (1080, 1920) # 1080p image size
 def handle_image(img_name):
   img_path = os.path.join(img_dir, img_name)
   image = cv2.imread(img_path)
-  if image is None:
-    return None
+  image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  '''
+  simple threshold
+  _, image = cv2.threshold(image, 110, 255, cv2.THRESH_BINARY)
+  '''
 
-  return resize_with_padding(image)
+  '''
+  gaussian threshold
+  '''
+  image = cv2.equalizeHist(image)
+  image = cv2.GaussianBlur(image, (5, 5), 0)
+  image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+  cv2.imshow('', image)
+  cv2.waitKey(0)
+  return image
 
 def resize_with_padding(image):
   height, width = image.shape[:2] # размер изображения
@@ -22,7 +34,7 @@ def resize_with_padding(image):
   new_height, new_width = int(height * scale), int(width * scale)
  
   # измененное изображение
-  resized_img = cv2.resize(image, (new_width, new_height))
+  resized_img = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
 
   # значения границ
   top = (target_size[0] - new_height) // 2
@@ -36,9 +48,9 @@ def main():
   extensions = ('.png', '.jpg', '.jpeg', '.bmp') # расширения изображений
   resized_dir = os.path.join(os.getcwd(), 'handled')
 
-  for filename in os.listdir('img'):
+  for filename in os.listdir(img_dir):
     if filename.lower().endswith(extensions):
-      cv2.imwrite(os.path.join(resized_dir, filename), handle_image(filename))
+      handle_image(filename)
 
 
 if __name__ == "__main__":
