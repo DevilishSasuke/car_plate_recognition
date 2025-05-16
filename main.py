40688084f45d088e790ce36c620554bf9f4a2050
DevilishSasuke <petrovich_uasya@mail.ru>
2025-05-16 03:13:04 +0300

    first async handle text version dirty code

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import cv2
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 from ultralytics import YOLO
-from typing import Optional, Dict
 from utils import *
+import time
 
-red = (0, 0, 255)
-green = (0, 255, 0)
-blue = (255, 0, 0)
-white = (255, 255, 255)
-acceptable_classes = [2, 6] # Допустимые классы, которые модель будет распознавать
-def detect_cars_and_buses_on_video(video_path: str, 
-                                   output_path: Optional[str] = "output.mp4", 
-                                   car_model_path: Optional[str] = "yolo11m.pt",
-                                   plate_model_path: Optional[str] = "model/plates.pt",
-                                   car_model: Optional[YOLO] = None,
-                                   plate_model: Optional[YOLO] = None):
+start_time = time.time()
+
+acceptable_classes = [2, 6]  # машины и автобусы
+executor = ThreadPoolExecutor(max_workers=2)
+plates_texts = []
+
+async def process_video(video_path: str, 
+                        car_model_path: Optional[str] = "yolo11m.pt",
+                        plate_model_path: Optional[str] = "model/plates.pt",
+                        car_model: Optional[YOLO] = None,
+                        plate_model: Optional[YOLO] = None):
   """
   Обнаруживает автомобили и автобусы на видео с помощью YOLO, выделяя их синим цветом.
   Обнаруживает номера автомобилей и автобусов на видео с помощью YOLO, выделяя их красным цветом.
 
   Args:
     video_path (str): Путь к входному видео.
-    output_path (str, optional): Путь для сохранения выходного видео. По умолчанию "output.mp4".
     car_model_path (str, optional): Путь к файлу модели YOLO. По умолчанию "yolo11m.pt".
     plate_model_path (str, optional) = Путь к файлу модели детекции номеров. По умолчанию: "model/plates.pt",
     car_model (YOLO, optional): Готовая модель для определения автомобилей на видео,
@@ -36,65 +37,87 @@ def detect_cars_and_buses_on_video(video_path: str,
 
   cap = cv2.VideoCapture(video_path)
   if not cap.isOpened():
-    raise ValueError(f"Неправильный путь к видео: {video_path}")
-  
-  out = create_video_writer(cap, output_path)
+      raise ValueError(f"Невозможно открыть видео: {video_path}")
 
-  car_names = car_model.names
-  # Чтение кадров с видео
+  tasks = []
+  frame_count = 0
+
   for frame in get_frames(cap):
-    processed_frame = process_frame(frame, car_model, plate_model, car_names)
+    frame_count += 1
 
-    # Запись обработанного кадра в выходное видео
-    out.write(processed_frame)
+    if frame_count % 3 != 0:
+      continue
 
-  # Закрытие объектов
-  out.release()
-  cv2.destroyAllWindows()
-  print(f"Готово. Видео сохранено в {output_path}")
+    # Обнаружение объектов с помощью YOLO
+    car_results = car_model.predict(source=frame, save=False)
 
-def process_frame(frame: np.ndarray, car_model: YOLO, plate_model: YOLO, 
-                  car_names:  Dict[int, str]) -> np.ndarray:
-  # Обнаружение объектов с помощью YOLO
-  car_results = car_model.predict(source=frame, save=False)
+    # Отображение обнаруженных автомобилей и автобусов на кадре
+    for cbox in get_boxes(car_results):
+      if cbox.cls.item() in acceptable_classes:
+        
+        x1, y1, x2, y2 = map(int, cbox.xyxy[0].tolist())
+        vehicle_img = frame[y1:y2, x1:x2]
 
-  # Отображение обнаруженных автомобилей и автобусов на кадре
-  for cbox in get_boxes(car_results):
-    if cbox.cls.item() in acceptable_classes:
-      
-      x1, y1, x2, y2 = map(int, cbox.xyxy[0].tolist())
-      vehicle_img = frame[y1:y2, x1:x2]
+        # Обнаружение номерных знаков на обнаруженных машинах
+        plate_img = detect_plates_in_vehicle(vehicle_img, plate_model)
+        if plate_img is None:
+          continue
+        plate_text = read_plate_text(plate_img)
+        if plate_text:
+          tasks.append(handle_plate_text(plate_text))
 
-      # Обнаружение номерных знаков на обнаруженных машинах
-      plate_results = plate_model.predict(source=vehicle_img, save=False, conf=0.7)
-      draw_rectangle(frame, cbox, label=car_names[int(cbox.cls.item())], color=blue)
-      for pbox in get_boxes(plate_results):
-        x1_, y1_, x2_, y2_ = map(int, pbox.xyxy[0].tolist())
-        plate_img = vehicle_img[y1_:y2_, x1_:x2_]
-        plate_text = get_plate_text(plate_img)
-        draw_rectangle(vehicle_img, pbox, label=plate_text, color=white)
+    if len(tasks) >= 10:  # пакетная обработка
+      await asyncio.gather(*tasks)
+      tasks.clear()
 
-  return frame
+  # Обработка оставшихся задач
+  if tasks:
+    await asyncio.gather(*tasks)
 
-def get_plate_text(plate_img: np.ndarray) -> str:
-  #plate_img = enlarge_image(plate_img)
+  with open("recognized_plates.txt", "w", encoding="utf-8") as f:
+    for plate in plates_texts:
+      if plate.strip():  # фильтрация пустых строк
+        f.write(plate + "\n")
+
+  end_time = time.time()
+  elapsed_time = end_time - start_time
+  print(f"The task took {elapsed_time:.2f} seconds to complete.")
+
+def detect_plates_in_vehicle(vehicle_img: np.ndarray, 
+                             plate_model: YOLO):
+  plate_results = plate_model.predict(source=vehicle_img, save=False, conf=0.7)
+  for pbox in get_boxes(plate_results):
+    x1_, y1_, x2_, y2_ = map(int, pbox.xyxy[0].tolist())
+    plate_img = vehicle_img[y1_:y2_, x1_:x2_]
+    return plate_img
+  return None
+
+async def handle_plate_text(text: str):
+  corrected_text = remove_incorrect_chars(text)
+  corrected_text = make_replacements(corrected_text) \
+    if len(corrected_text) >= 6 else corrected_text
+  plates_texts.append(corrected_text)
+  return corrected_text
+
+async def async_recognize_plate(plate_img):
+  result = await asyncio.get_event_loop().run_in_executor(executor, sync_recognize_plate, plate_img)
+  plates_texts.append(result)
+  return
+
+def sync_recognize_plate(plate_img):
+  plate_img = enlarge_image(plate_img)
   text = read_plate_text(plate_img)
-
-  if text != "":
+  
+  if text:
     corrected_text = remove_incorrect_chars(text)
-    if len(corrected_text) >= 6:
-      return make_replacements(corrected_text)
-    return corrected_text
-  return text
+    return make_replacements(corrected_text) \
+      if len(corrected_text) >= 6 else corrected_text
+  return ""
 
 
 if __name__ == "__main__":
-    video_path = "test/test2.mp4" 
-    output_path = "test/output.mp4"
-    car_model_path= "model/yolo11m.pt"
-    plate_model_path = "model/plates.pt"
-    detect_cars_and_buses_on_video(video_path=video_path,
-                                   output_path=output_path,
-                                   car_model_path=car_model_path,
-                                   plate_model_path=plate_model_path,
-                                   )
+  asyncio.run(process_video(
+    video_path="test/test2.mp4",
+    car_model_path="model/yolo11m.pt",
+    plate_model_path="model/plates.pt"
+  ))
--- utils.py
+++ utils.py
@@ -5,20 +5,11 @@ from ultralytics.engine.results import Boxes, Results
 from paddleocr import PaddleOCR
 from re import sub as re_sub
 
-min_ph = 200 # минимальная высота номера(для увеличения)
-min_pw = 600 # минимальная ширина номера
+min_ph = 40 # минимальная высота номера(для увеличения)
+min_pw = 120 # минимальная ширина номера
 plate_alphabet = "ABEKMHOPCTYX0123456789" # алфавит доступных номерных символов
-reader = PaddleOCR(use_angle_cls=False, lang='en') # объект для чтения текста с изображения
-
-def create_video_writer(cap: cv2.VideoCapture, output_path: str) -> cv2.VideoWriter:
-  # Параметры видео
-  frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-  frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-  fps = cap.get(cv2.CAP_PROP_FPS)
-
-  # Создание объекта для записи видео
-  fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-  return cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
+reader = PaddleOCR(use_angle_cls=False, 
+                   lang='en', ) # объект для чтения текста с изображения
 
 def get_frames(cap: cv2.VideoCapture) -> Generator[np.ndarray, None, None]:
   while cap.isOpened():
@@ -30,18 +21,6 @@ def get_frames(cap: cv2.VideoCapture) -> Generator[np.ndarray, None, None]:
   cap.release()
   return
 
-def draw_rectangle(frame: np.ndarray,
-                    box: Boxes, 
-                    label: Optional[str] = "none",
-                    color: Optional[Tuple[int, int, int]] = (0, 255, 0)):
-  
-  x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
-  confidence = box.conf[0].item()
-  label = f"{label}: {confidence:.2f}"
-
-  cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
-  cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
-
 def get_boxes(results: Results) -> Generator[Boxes, None, None]:
   for result in results:
             for box in result.boxes:
@@ -50,16 +29,20 @@ def get_boxes(results: Results) -> Generator[Boxes, None, None]:
   return
 
 def enlarge_image(img: np.ndarray) -> np.ndarray:
-  h, w, _ = img.shape
-  scale = max(min_pw / w, min_ph / h)
+    h, w = img.shape[:2]
+    scale = max(min_pw / w, min_ph / h)
 
-  if scale < 1:
-    return img
-  
-  return cv2.resize(img, (int(w * scale), int(h * scale)), interpolation=cv2.INTER_AREA)
+    if scale <= 1:
+        return img
+
+    new_w, new_h = int(w * scale), int(h * scale)
+    return cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
+
 
 def read_plate_text(plate_img: np.ndarray) -> str:
   texts = reader.ocr(plate_img, cls=False)
+  print(f"НАЙДЕННЫЙ ТЕКСТ: {texts}")
+
   result  = ""
 
   if not texts or not texts[0]:
@@ -74,9 +57,7 @@ def read_plate_text(plate_img: np.ndarray) -> str:
 def remove_incorrect_chars(plate_text: str) -> str:
   return re_sub(fr"[^{plate_alphabet}]", "", plate_text).strip()
 
-replacements = {
-    "0": "O", "8": "B", "2": "Z",
-  }
+replacements = { "0": "O", "8": "B", "2": "Z", }
 def make_replacements(text: str) -> str:
   letters = text[0] + text[4:6]
   numbers = text[1:4]
